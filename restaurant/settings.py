fa32e729c2a4d2f2aa6a7c0d2be2d75ebd8320a0
Blommis <blomquist799@gmail.com>
2025-05-21 11:13:26 +0200

    installed static and collectstatic

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'): # This file does not exist on the deployed version
     import env
-    
+
 
 
 
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com',
                  '.127.0.0.1',]
@@ -140,6 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
