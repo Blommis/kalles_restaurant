06cdb93d215f212d54981e2413f0bdc2ac803355
Blommis <blomquist799@gmail.com>
2025-05-26 14:34:38 +0200

    created eventlistener to Hide mobile navbar on nav link click

--- static/js/script.js
+++ static/js/script.js
@@ -23,8 +23,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  // Initiera kartan när API-laddningen är klar
-   function initMap() { 
+  
+function initMap() { 
     let options ={ 
         zoom:8, 
         center:{lat: 42.3601, lng:-71.0589}
@@ -32,9 +32,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let map = new google.maps.Map(document.getElementById('map'), options);
 
-    const marker = new google.maps.AdvancedMarkerElement({
+    const marker = new google.maps.Marker({
         position: { lat: 42.4668, lng: -70.9495 },
         map,
         title: "Restaurant"
     });
 };
+
+// to make sure Navbar collapses in mobile device
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".navbar-collapse .nav-link").forEach((link) => {
+        link.addEventListener("click", function () {
+            let navbarCollapse = document.querySelector(".navbar-collapse");
+            let bsCollapse = new bootstrap.Collapse(navbarCollapse); 
+            bsCollapse.hide(); 
+        });
+    });
+});
--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -23,20 +23,29 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
- 
-    function initMap() {
-        let options ={
-            zoom:8,
-            center:{lat: 42.3601, lng:-71.0589}
-        }
-        let map = new google.maps.Map(document.getElementById('map'), options);
+  // Initiera kartan när API-laddningen är klar
+function initMap() { 
+    let options ={ 
+        zoom:8, 
+        center:{lat: 42.3601, lng:-71.0589}
+     }
 
-        // Add marker
-        let marker = new google.maps.Marker({
-            position:{lat:42.4668, lng:-70.9495},
-                map: map
-            
+    let map = new google.maps.Map(document.getElementById('map'), options);
+
+    const marker = new google.maps.Marker({
+        position: { lat: 42.4668, lng: -70.9495 },
+        map,
+        title: "Restaurant"
+    });
+};
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".navbar-collapse .nav-link").forEach((link) => {
+        link.addEventListener("click", function () {
+            let navbarCollapse = document.querySelector(".navbar-collapse");
+            let bsCollapse = new bootstrap.Collapse(navbarCollapse); 
+            bsCollapse.hide(); // Stänger menyn när en länk klickas
         });
-
-
-    }
+    });
+});
