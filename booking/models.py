4bd90f35b75e16894de63bcea53c8d1073b1a1ab
Blommis <blomquist799@gmail.com>
2025-05-26 20:53:07 +0200

    fixed booking_code to because too long sentence

--- booking/models.py
+++ booking/models.py
@@ -1,13 +1,14 @@
-from django.db import models 
+from django.db import models
 import uuid
 # Create your models here.
 
 
 class Reservation(models.Model):
-    booking_code = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    booking_code = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
     name = models.CharField(max_length=200)
     date = models.DateField()
     time = models.TimeField()
     guests = models.PositiveIntegerField()
-
-
