37b0a940af61036b8f9eaf8cb84d7875b46ebf3e
Blommis <blomquist799@gmail.com>
2025-05-26 13:11:56 +0200

    changed to AdvancedMarkerElement due to warnings in dev tools

--- static/js/script.js
+++ static/js/script.js
@@ -24,19 +24,17 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // Initiera kartan när API-laddningen är klar
-    function initMap() {
-        let options ={
-            zoom:8,
-            center:{lat: 42.3601, lng:-71.0589}
-        }
-        let map = new google.maps.Map(document.getElementById('map'), options);
+   function initMap() { 
+    let options ={ 
+        zoom:8, 
+        center:{lat: 42.3601, lng:-71.0589}
+     }
 
-        // Add marker
-        let marker = new google.maps.Marker({
-            position:{lat:42.4668, lng:-70.9495},
-                map: map
-            
-        });
+    let map = new google.maps.Map(document.getElementById('map'), options);
 
-
-    }
+    const marker = new google.maps.AdvancedMarkerElement({
+        position: { lat: 42.4668, lng: -70.9495 },
+        map,
+        title: "Restaurant"
+    });
+};
