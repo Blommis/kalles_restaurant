201714d12abf9da6e2956896c80f18f30fb381e7
Blommis <blomquist799@gmail.com>
2025-05-28 15:52:05 +0200

    changed to advanced marker element from 'marker' function due to dev tools warnings

--- static/js/script.js
+++ static/js/script.js
@@ -26,20 +26,21 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   
-function initMap() { 
-    let options ={ 
-        zoom:8, 
-        center:{lat: 42.3601, lng:-71.0589}
-     }
-     // 'google' is a global object provided by the Google Maps API script
-    let map = new google.maps.Map(document.getElementById('map'), options);
+async function initMap() {
+      const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-    const marker = new google.maps.Marker({
+      const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: { lat: 42.3601, lng: -71.0589 },
+        mapId: "9c695404f34e7d4bb115f515"
+      });
+
+      const marker = new AdvancedMarkerElement({
+        map: map,
         position: { lat: 42.4668, lng: -70.9495 },
-        map,
-        title: "Restaurant"
-    });
-};
+        title: "Kalle's",
+        });
+      }
 
 // to make sure Navbar collapses in mobile device
 document.addEventListener("DOMContentLoaded", function () {
