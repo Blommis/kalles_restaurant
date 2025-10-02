74eabff1c7bae220c4ff1b3c3a10cdf36bda9745
Blommis <blomquist799@gmail.com>
2025-10-02 11:14:18 +0200

    Added signals.py to send welcome email on user signup

---
+++ booking/signals.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=User)
+def send_welcome_email(sender, instance, created, **kwargs):
+    if created:  # only when new user creates
+        subject = "Welcome to Kalle's Restaurant!"
+        message = (
+            f"Hi {instance.username},\n\n"
+            "Thanks for signing up at Kalle's Restaurant.\n"
+            "You can now manage your bookings, update details, and never miss your table!\n\n"
+            "– Kalle's Team"
+        )
+        send_mail(subject, message, settings.EMAIL_HOST_USER, [instance.email])
